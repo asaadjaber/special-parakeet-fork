0afcc1390d8d89fa503828dc59f90fd02f748be2
Asaad Jaber <asaadmjaber@gmail.com>
2024-01-24 11:35:37 +0200

    implementing a test from an SO post replacing lines 18 and 19 with a call to assert succeeds fails

--- Special Parakeet/firestore-test/firestore.test.js
+++ Special Parakeet/firestore-test/firestore.test.js
@@ -2,18 +2,40 @@
  * @jest-environment node
  */
 
-import {
-  assertFails,
-  assertSucceeds,
-  initializeTestEnvironment,
-  RulesTestEnvironment,
-} from "@firebase/rules-unit-testing"
+const { initializeTestEnvironment, assertSucceeds, assertFails} = require("@firebase/rules-unit-testing");
+const { doc, getDoc } = require("firebase/firestore");
+const fs = require('fs');
 
-let testEnv = await initializeTestEnvironment({
-  projectId: "special-parakeet",
-  firestore: {
-    rules: fs.readFileSync("special-parakeet-firestore.rules", "utf8"),
-  },
+const testRules = async () => {
+    const testEnv = await initializeTestEnvironment({
+        projectId: "special-parakeet",
+        firestore: {
+            rules: fs.readFileSync("special-parakeet-firestore.rules", "utf8"),
+            host: "localhost", // Required to run in the emulator in case it’s not automatically detected!
+            port: 8080
+        },
+    });
+    firebase.assertSucceeds(app.firestore().collection("isFavorited").doc("bird").get());
+    // const context = await testEnv.authenticatedContext("id_alice");
+    // return await assertSucceeds(getDoc(doc(context.firestore(), "users", "id_alice")));
+}
+
+test("Simulated read is allowed by Firebase rules", async () => {
+    await expect(testRules()).resolves.toBeDefined(); // assertSucceeds promise is resolved since rules allowed it
 });
 
-firebase.assertSucceeds(app.firestore().collection("isFavorited").doc("bird").get());
+// import {
+//   assertFails,
+//   assertSucceeds,
+//   initializeTestEnvironment,
+//   RulesTestEnvironment,
+// } from "@firebase/rules-unit-testing"
+
+// let testEnv = await initializeTestEnvironment({
+//   projectId: "special-parakeet",
+//   firestore: {
+//     rules: fs.readFileSync("special-parakeet-firestore.rules", "utf8"),
+//   },
+// });
+
+// firebase.assertSucceeds(app.firestore().collection("isFavorited").doc("bird").get());
